8aa85cd3c68e7cd297b74363829936edadb128ac
jaspreet <jaspreetsangha204@yahoo.com>
2023-06-05 17:41:26 -0700

    Feature 15 complete

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
-# from tasks.models import Task
-# from django.contrib.auth.decorators import login_required
+from tasks.models import Task
+from django.contrib.auth.decorators import login_required
+from tasks.forms import TaskForm
 
-# # Create your views here.
-# @login_required
-# def tasks_list(request):
-#     tasks = Task.objects.filter(assignee=request.user)
-#     context = {
-#         "tasks": tasks
-#     }
-#     return render(request, "tasks/list.html")
+# Create your views here.
+@login_required
+def create_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list_projects")
+    else:
+        form = TaskForm(request.POST)
+    context = {
+        "form": form
+    }
+    return render(request, "tasks/create.html", context)
